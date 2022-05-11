d1b18cb488fdab60fe8c3beec299c34fce5d2853
Alexander Bulatov <aleks40k@yandex.ru>
2022-05-11 22:38:51 +0300

    fix: 1. В InMemoryHistoryManager добавлена проверка полученной задачи на null перед добавлением в историю. 2. Реализация tasksHistory изменена на LinkedList. 3. Для поля HISTORY_MAX_SIZE добавлен модификатор static.

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -2,25 +2,27 @@ package managers;
 
 import tasks.Task;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> tasksHistory = new ArrayList<>();
-    private final int HISTORY_MAX_SIZE = 10;
+    private final List<Task> tasksHistory = new LinkedList<>();
+    private final static int HISTORY_MAX_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        tasksHistory.add(task);
+        if (task != null) {
+            tasksHistory.add(task);
 
-        if (tasksHistory.size() > HISTORY_MAX_SIZE) {
-            tasksHistory.remove(0);
+            if (tasksHistory.size() > HISTORY_MAX_SIZE) {
+                tasksHistory.remove(0);
+            }
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(tasksHistory);
+        return new LinkedList<>(tasksHistory);
     }
 }
